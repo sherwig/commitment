e18876ce53c12b5939bdb38c08b50793d0dd4a54
Sam <>
2021-02-02 10:20:01 -0700

    97

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@
 
 function javaBoi()
 {
-  var divvy=document.getElementsByClassName(thought11);
-  console.log(divvy);
+  var divvy=document.getElementsByClassName('thought11');
+  var heady = document.createElement('H1');
+  heady.innerHTML = "Added this with JS!";
+  // divvy.innerHTML = "Added this with JS!";
+  // console.log(divvy);
 
 
 }
+
+javaBoi();
